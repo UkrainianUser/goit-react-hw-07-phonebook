6cc6eda1f6876ac83bece740160d5e57be271da8
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-05-22 00:47:27 +0300

    start up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-export const App = () => {
+import css from "./App.module.css";
+import ContactForm from "./contactForm/ContactForm";
+import Filter from "./filter/Filter";
+import ContactList from "./contactList/ContactList"
+
+export default function App () {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.phonebook}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
     </div>
   );
 };
---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,18 @@
+import css from "./Filter.module.css";
+import { useDispatch } from "react-redux";
+import { setFilter } from "redax/filterSlice";
+
+const Filter = () => {
+  const dispatch = useDispatch();
+
+  const onFilterChange = (evt) => dispatch(setFilter(evt.currentTarget.value));
+
+  return (
+    <label className={css.label}>
+      Find contacts by name
+      <input className={css.input} type="text" name="filter" onChange={onFilterChange} />
+    </label>
+  );
+};
+
+export default Filter;
